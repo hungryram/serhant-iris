1ca80e6849ce49758f1284af0b86c9d83b2d3252
Ram Dettmer <ramdettmer751@gmail.com>
2022-04-24 19:18:31 -0700

    update thank-you

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -1,11 +1,16 @@
 import * as React from "react"
 import Layout from "../components/Layout"
 import { StaticImage } from "gatsby-plugin-image"
+import Seo from "../components/Seo"
 
 
 export default function Thanks() {
     return (
         <Layout>
+            <Seo
+                title="Thank you"
+                description="Our team will review your inquiry and reach out as soon as possible"
+            />
                 <StaticImage
                     src="../../static/images/lviing-room-iris-tribeca.jpg"
                     alt="19 Park Place living room"
